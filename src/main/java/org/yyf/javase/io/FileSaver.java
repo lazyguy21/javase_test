43481fae7f07e57add89cbe3f51e602df9e8ffe6
ye <yyf@xiaomaihd.com>
2015-08-13 11:21:02 +0800

    FileSaver

---
+++ src/main/java/org/yyf/javase/io/FileSaver.java
@@ -0,0 +1,19 @@
+package org.yyf.javase.io;
+
+import java.io.*;
+
+/**
+ * Created by ye on 15-8-13.
+ */
+public class FileSaver {
+    public static void main(String[] args) throws IOException {
+        String str = "testdasdfadf";
+        File test = new File("Testt.txt");
+        test.createNewFile();
+        FileWriter fileWriter = new FileWriter(test);
+        fileWriter.write(str.toCharArray());
+        fileWriter.close();
+
+
+    }
+}
