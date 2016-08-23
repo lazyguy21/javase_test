fe12bd508fe1521c563544a3ea5e8d3c122f836d
lazyguy21 <15882041972@sina.cn>
2016-08-23 18:08:51 +0800

    java并不会将null默认为false，这一点跟大多数语言并不一样

---
+++ src/main/java/org/yyf/javase/NullCompareTest.java
@@ -0,0 +1,18 @@
+package org.yyf.javase;
+
+/**
+ * Created by tobi on 16-8-23.
+ */
+public class NullCompareTest {
+    public static void main(String[] args) {
+        Boolean a = null;
+        System.out.println(a);
+        /*
+        if(a){//java并不会将null默认为false，这一点跟大多数语言并不一样
+            System.out.println(a);
+        }
+        */
+//        System.out.println(true==a);//同理
+
+    }
+}
