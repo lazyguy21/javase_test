283a9259f8c83af8cde6ff40c918c0d41e1b87a5
lazyguy21 <15882041972@sina.cn>
2016-09-26 10:52:36 +0800

    TimeUnit convert

---
+++ src/main/java/org/yyf/javase/util/TimeUnitTest.java
@@ -0,0 +1,13 @@
+package org.yyf.javase.util;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by tobi on 16-9-26.
+ */
+public class TimeUnitTest {
+    public static void main(String[] args) {
+        long durationInSeconds = TimeUnit.SECONDS.convert(1L, TimeUnit.MINUTES);
+        System.out.println(durationInSeconds);
+    }
+}
